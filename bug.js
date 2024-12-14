952f132a857dc0055fed221fbe2964e3aa7799aa
BugHunter-X <askzenai@gmail.com>
2024-12-14 13:50:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase Realtime Database sometimes throws a `FirebaseError: Quota exceeded` error even when the project's usage is well below the free tier limits. This error is often intermittent and difficult to track down.
